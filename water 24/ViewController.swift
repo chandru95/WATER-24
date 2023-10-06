6b3c9a2b4830917fb5ae66b5406c5adc82bfd166
chandru95 <chandru041195@gmail.com>
2023-10-06 19:28:58 +0530

    Initial Commit

---
+++ water 24/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  water 24
+//
+//  Created by Admin on 06/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
